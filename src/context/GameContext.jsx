7ad7d5653dac703a74f4f6fef2fad95e213c1298
Barış Kahraman <baris-kahra@hotmail.com>
2024-06-26 04:16:42 +0300

    feat: create GameProvider with initial state management

--- src/context/GameContext.jsx
+++ src/context/GameContext.jsx
@@ -1,3 +1,13 @@
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 
 export const GameContext = createContext();
+
+export const GameProvider = ({children}) => {
+  const [board , setBoard] = useState([...Array.fill(null)]);
+  const [nextPlayer, setNextPlayer] = useState('X');
+  const [hasWinner, setHasWinner] = useState(false);
+
+  return (
+    <GameContext.Provider value={{}}>{children}</GameContext.Provider>
+  )
+}
